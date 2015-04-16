dacd3e2b637138fc2886993b2f8d55ae9b9e5413
karandeepdps <karandeepdps@gmail.com>
2015-04-16 14:30:15 +0530

    Now it displays all data
    Added associate array to display whole db.
    Now can use if condition.

--- sap.php
+++ sap.php
@@ -6,10 +6,10 @@ $password="";
 $servername="localhost";
 $database="KARAN";
 $conn=new mysqli($servername,$username,$password,$database);
-$sql="Insert into SAP(Name,SAP) values ('$x','$y')";
+$sql="Insert into SAP(Name,Sap) values ('$x','$y')";
 if($conn->query($sql)=== TRUE)
 {
-	echo "Inserted Succesfully $x with sap id $y";
+	echo "Inserted Succesfully $x with sap id $y <br>";
 	//echo $x."Inserted succesfully with sap id".$y;
 }
 else
@@ -17,6 +17,13 @@ else
 	echo $conn->error;
 }
 $k="SELECT * FROM SAP";
-$row=$conn->query($k)->fetch_assoc();
-echo $row["Name"];
+$result=$conn->query($k);
+
+while($row=$result->fetch_assoc())
+{
+	if($row['Sap']=='5000')
+	echo $row['Name']."&nbsp";
+echo $row['Sap']."<br>";
+}
+
 ?>
